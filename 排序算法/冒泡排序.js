d961f2fb760a85747fdb59bd843300b1a7701f52
kevinzzzzz <jie736805161@qq.com>
2023-07-31 23:45:53 +0800

    feat: 冒泡排序

---
+++ 排序算法/冒泡排序.js
@@ -0,0 +1,14 @@
+let BobbleSort = function (arr, flag = 0) {
+  let len = arr.length
+  for (let i = 0; i < len - 1; i++) { // 设置指针一的边界
+    for (let j = 0; j < len - 1 - i; j++) { // 设置指针二的边界，且不能超过指针一
+      if (arr[j] > arr[j + 1]) { // 当比较前者大于后者， 相互换位
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return flag ? arr.reverse() : arr
+}
+
+let arr = [2, 9, 6, 7, 4, 3, 1, 7]
+console.log(BobbleSort(arr, 1))
