7981878ea16fc643b364992eb7f846d44f94a0a2
kevinzzzzz <jie736805161@qq.com>
2023-09-26 23:38:30 +0800

    feat: 20230926save

--- 58.最后一个单词的长度.js
+++ 58.最后一个单词的长度.js
@@ -2,6 +2,7 @@
  * @lc app=leetcode.cn id=58 lang=javascript
  *
  * [58] 最后一个单词的长度
+ * 思路  将字符串反过来便利查询
  */
 
 // @lc code=start
@@ -10,7 +11,12 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-
+  let a = 0
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] != ' ') a++
+    if (s[i] == ' ' && a) break
+  }
+  return a
 };
 // @lc code=end
 
