171ce0a328b23a08f8c8483867c66835562ba4a3
kevinzzzzz <jie736805161@qq.com>
2023-12-05 23:54:30 +0800

    feat: save

---
+++ 55.跳跃游戏.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=javascript
+ *
+ * [55] 跳跃游戏
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let n = nums.length
+  // 当前所能跳转的最大步数
+  let farthest = 0
+  for(let i = 0; i < nums.length - 1; i++) {
+    // 更新当前所能跳转的最大步数
+    farthest = Math.max(farthest, nums[i] + i)
+    // 
+    if (farthest <= i) return false
+  }
+  // 判断是否能到达最后一位的下标
+  return farthest >= n - 1
+};
+// @lc code=end
+
