08f79eb503751043dfd64eff53fc9fa623c9c1f1
kevinzzzzz <jie736805161@qq.com>
2023-12-07 23:44:48 +0800

    feat: 跳跃问题

--- 55.跳跃游戏.js
+++ 55.跳跃游戏.js
@@ -13,10 +13,9 @@ var canJump = function(nums) {
   let n = nums.length
   // 当前所能跳转的最大步数
   let farthest = 0
-  for(let i = 0; i < nums.length - 1; i++) {
-    // 更新当前所能跳转的最大步数
+  for(let i = 0; i < n - 1; i++) {
+    // 更新当前所能跳转的最大步数 nums[i] + i表示你最远能跳到下标几
     farthest = Math.max(farthest, nums[i] + i)
-    // 
     if (farthest <= i) return false
   }
   // 判断是否能到达最后一位的下标
