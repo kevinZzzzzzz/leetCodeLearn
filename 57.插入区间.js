4f5f64e1cd1b8799841532883304c3b8276c1016
kevinzzzzz <jie736805161@qq.com>
2024-01-02 22:49:42 +0800

    save

---
+++ 57.插入区间.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=57 lang=javascript
+ *
+ * [57] 插入区间
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+  if (!intervals.length) return newInterval
+};
+// @lc code=end
+
