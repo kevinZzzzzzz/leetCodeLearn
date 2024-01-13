6f3a8d52d0697ccfe713d25902f695769c156983
kevinzzzzz <jie736805161@qq.com>
2024-01-13 23:17:29 +0800

    feat: 搜索二维矩阵

---
+++ 74.搜索二维矩阵.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=javascript
+ *
+ * [74] 搜索二维矩阵
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  let flag = false
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === target) {
+        flag = true
+      }
+    }
+  }
+  return flag
+};
+// @lc code=end
+
