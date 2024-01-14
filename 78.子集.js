7d9a84efc118d8875a5bcaed3f2b06c7e9b3dc4b
kevinzzzzz <jie736805161@qq.com>
2024-01-14 22:53:13 +0800

    feat: 子集

---
+++ 78.子集.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=javascript
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  let res = []
+  let track = []
+  const backtrack = (nums, start, track) => {
+    res.push([...track])
+    for (let i = start; i < nums.length; i++) {
+      track.push(nums[i])
+      backtrack(nums, i + 1, track)
+      track.pop() // 选择回溯
+    }
+  }
+  backtrack(nums, 0, track)
+  return res
+};
+// @lc code=end
+
